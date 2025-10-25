4eeaf46f2ee3fbfa4bbcd00aa417d44295e0b4fd
John Patrick Marcellana <jptmarcellana@gmail.com>
2025-10-25 22:15:43 +0800

    added .env

--- cita/firebase/firebase.js
+++ cita/firebase/firebase.js
@@ -1,22 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArm1v0Dz-wzlo7y7FmoYDsJiyrQmcVYnw",
-  authDomain: "cita-dating-app.firebaseapp.com",
-  projectId: "cita-dating-app",
-  storageBucket: "cita-dating-app.firebasestorage.app",
-  messagingSenderId: "301139807979",
-  appId: "1:301139807979:web:92e64f49441d05cf0478ae",
-  measurementId: "G-2PSV6KB6K2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
